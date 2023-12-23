f8f1f4706e96ecaec2b02c58fa70fd45de10355a
Om Prakash Mallik <opm202000@gmail.com>
2023-12-23 19:16:03 +0530

    done

--- OOPs2/src/Main.java
+++
@@ -1,9 +0,0 @@
-import Encapsulation.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Account ac = new Account(1215, "opm", 54685, 6548);
-        ac.checkBalance();
-        ac.debitBalance(666600);
-    }
-}
